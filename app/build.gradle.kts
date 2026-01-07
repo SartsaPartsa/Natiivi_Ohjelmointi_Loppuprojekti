9c3ec182b86e61cdc51238baf87dbe9a590c7317
Sara Vehviläinen <sara@vehvilainen.cc>
2026-01-07 16:06:45 +0200

    Korjattu virheet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.triviaquiz"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.triviaquiz"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
